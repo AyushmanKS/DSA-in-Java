6577d630c55b4d13d782ac5c194ea38a45882ec1
AyushmanKS <ayushmanks1845@gmail.com>
2024-06-14 13:59:14 +0530

    removeLast in LinkedList

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -91,6 +91,28 @@ public class LinkedList1 {
         return val;
     }
 
+    public int removeLast() {
+        if(size == 0) {
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }
+        else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        // prev:  i = size-2
+        Node prev = head;
+        for(int i=0; i<size-2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
     public static void main(String args[]) {
         LinkedList1 ll = new LinkedList1();
         ll.print();
@@ -103,6 +125,7 @@ public class LinkedList1 {
         ll.addLast(4);
         ll.add(2, 10);
         ll.removeFirst();
+        ll.removeLast();
         ll.print(); 
         System.out.println("Size of LinkedList: "+size);   
     }
