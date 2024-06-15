a6bb0b6392be5f4b971ec3607b1898a1c350a825
AyushmanKS <ayushmanks1845@gmail.com>
2024-06-15 13:13:34 +0530

    delete Nth Node from End in LinkedList

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -158,6 +158,28 @@ public class LinkedList1 {
         }
         head = prev;
     }
+
+    public void deleteNthfromEnd(int n) {
+        int sz = 0;
+        Node temp = head;
+        while(temp != null) {
+            temp = temp.next;
+            sz++;
+        }
+        if(n == sz) {
+            head = head.next;
+            return;
+        }
+        int i = 1;
+        int iToFind = sz - n;
+        Node prev = head;
+        while(i < iToFind) {
+            prev = prev.next;
+            i++;
+        } 
+        prev.next = prev.next.next;
+        return;
+    }
     public static void main(String args[]) {
         LinkedList1 ll = new LinkedList1();
         ll.print();
@@ -175,7 +197,7 @@ public class LinkedList1 {
         System.out.println("Size of LinkedList: "+size); 
         System.out.println("Element found at index: "+ll.iterativeSearch(3));
         System.out.println("Element found at index: "+ll.recursiveSearch(3));  
-        ll.reverse();
+        ll.deleteNthfromEnd(3);
         ll.print();
     }
 }
