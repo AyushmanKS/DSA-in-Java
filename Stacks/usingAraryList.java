2cdbf0b985c7fe1333cc1c5aa610e87e7576ea81
AyushmanKS <ayushmanks1845@gmail.com>
2024-07-21 17:12:33 +0530

    implement stack using ArrayList

---
+++ Stacks/usingAraryList.java
@@ -0,0 +1,44 @@
+package Stacks;
+
+import java.util.ArrayList;
+
+public class usingAraryList {
+    static class Stack {
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty() {
+            return list.size() == 0;
+        }
+        // push
+        public static void push(int data) {
+            list.add(data);
+        }
+        // pop: LIFO
+        public static int pop() {
+            if(isEmpty()) {
+                return -1;
+            }
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+        // Peek
+        public static int peek() {
+            return list.get(list.size() - 1);
+        } 
+    }
+    public static void main(String args[]) {
+        Stack s = new Stack();
+        Stack.push(0);
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5); 
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
