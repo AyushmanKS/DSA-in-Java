20a0402e016a71f0718004e9ed357f513f8a2ecf
AyushmanKS <ayushmanks1845@gmail.com>
2024-09-05 16:12:58 +0530

    implement Stack using ArrayList

--- Stacks/Stack.java
+++ Stacks/Stack.java
@@ -2,10 +2,12 @@ package Stacks;
 
 import java.util.ArrayList;
 
-public class usingAraryList {
-    static class Stack {
+// implementing stack using arrayList
+public class Stack {
+    static class StackA {
         static ArrayList<Integer> list = new ArrayList<>();
-
+        
+        // is no element return true
         public static boolean isEmpty() {
             return list.size() == 0;
         }
@@ -13,32 +15,36 @@ public class usingAraryList {
         public static void push(int data) {
             list.add(data);
         }
-        // pop: LIFO
+        // pop
         public static int pop() {
             if(isEmpty()) {
                 return -1;
             }
-            int top = list.get(list.size() - 1);
-            list.remove(list.size() - 1);
+
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
             return top;
         }
-        // Peek
+        // peek
         public static int peek() {
-            return list.get(list.size() - 1);
+            if(isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size()-1);
         } 
     }
     public static void main(String args[]) {
-        Stack s = new Stack();
-        Stack.push(0);
+        StackA s = new StackA();
+
         s.push(1);
         s.push(2);
         s.push(3);
         s.push(4);
-        s.push(5); 
 
-        while (!s.isEmpty()) {
+        while(!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
         }
     }
+    
 }
