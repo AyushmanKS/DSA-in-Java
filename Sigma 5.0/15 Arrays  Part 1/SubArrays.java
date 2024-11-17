9219a661d5a7a2e57e02647c73bdb4423c09b4d3
AyushmanKS <ayushmanks1845@gmail.com>
2024-11-17 16:58:14 +0530

    sum of subarray elements

--- Sigma 5.0/15 Arrays  Part 1/SubArrays.java
+++ Sigma 5.0/15 Arrays  Part 1/SubArrays.java
@@ -2,11 +2,15 @@ public class SubArrays {
     
     public static void subArrays(int arr[]) {
         int l = arr.length;
+        int sum = 0;
         for(int i=0; i<l; i++) {
             for(int j=i+1; j<l; j++) {
                 for(int k=i; k<=j; k++) {
                     System.out.print(arr[k] + " ");
+                    sum+=arr[k];
                 }
+                System.out.print("      Sum is:" + sum);
+                sum = 0;
                 System.out.println();
             }
             System.out.println();
