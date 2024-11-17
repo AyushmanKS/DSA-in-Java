4d2ba5f65c8de709282fd70a63213b1af89bac7e
AyushmanKS <ayushmanks1845@gmail.com>
2024-11-17 16:17:19 +0530

    reverse array

---
+++ Sigma 5.0/15 Arrays  Part 1/ReverseArray.java
@@ -0,0 +1,24 @@
+public class ReverseArray {
+    
+    public static void reverseArray(int arr[]) {
+
+        int i = 0;
+        int j = arr.length-1;
+
+        while(i < j) {
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+            i++;
+            j--;
+        } 
+
+        for(int p=0; p<=arr.length-1; p++) {
+            System.out.print(arr[p]+" ");
+        }
+    }
+    public static void main(String args[]) {
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        reverseArray(arr);
+    }
+}
