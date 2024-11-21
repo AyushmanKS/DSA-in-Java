c3184ab83a06da4daf32c5130b751b3ddd0e2cf2
AyushmanKS <ayushmanks1845@gmail.com>
2024-11-21 22:00:31 +0530

    buy and sell stocks

---
+++ Sigma 5.0/16 Arrays Part 2/BuySellStockes.java
@@ -0,0 +1,22 @@
+public class BuySellStockes {
+    
+    public static int buySellStocks(int arr[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            if(buyPrice < arr[i]) {
+                int profit =  arr[i] - buyPrice; 
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            else {
+                buyPrice = arr[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String args[]) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println("Max profit is: "+buySellStocks(prices));
+    }
+}
