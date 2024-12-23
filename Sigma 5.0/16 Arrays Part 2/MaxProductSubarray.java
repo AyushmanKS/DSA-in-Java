0929f076a75f8c5e64fe63f2a0102d21cfaa0c54
AyushmanKS <ayushmanks1845@gmail.com>
2024-12-23 14:50:13 +0530

    maximum product subarray

---
+++ Sigma 5.0/16 Arrays Part 2/MaxProductSubarray.java
@@ -0,0 +1,25 @@
+public class MaxProductSubarray {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int prefix = 1;
+        int suffix = 1;
+        int ans = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++) {
+            if (prefix == 0) prefix = 1;
+            if (suffix == 0) suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n - i - 1];
+
+            ans = Math.max(ans, Math.max(prefix, suffix));
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        MaxProductSubarray mps = new MaxProductSubarray();
+        int nums [] = {2,3,-2,4};
+        System.out.println(mps.maxProduct(nums));
+    }
+}
