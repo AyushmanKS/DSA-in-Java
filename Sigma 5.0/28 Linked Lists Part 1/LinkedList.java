75e102a86153253c0cd32ac4a7977c44f4fc8478
AyushmanKS <ayushmanks1845@gmail.com>
2025-01-10 20:07:48 +0530

    even odd order in LL

--- Sigma 5.0/28 Linked Lists Part 1/LinkedList.java
+++ Sigma 5.0/28 Linked Lists Part 1/LinkedList.java
@@ -140,6 +140,25 @@ public class LinkedList {
         prev.next = null;
     }
 
+    public static Node evenOddOrder(Node head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        Node odd = head;
+        Node even = head.next;
+        Node evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next; 
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+    
     public void printLL() {
         Node temp = head;
         while(temp != null) {
