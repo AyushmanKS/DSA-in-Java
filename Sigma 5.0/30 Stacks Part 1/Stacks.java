1fb51611e4216e426db94b3fb98e5cdd22ba9742
AyushmanKS <ayushmanks1845@gmail.com>
2025-01-12 18:55:29 +0530

    stack push at bottom

--- Sigma 5.0/30 Stacks Part 1/Stacks.java
+++ Sigma 5.0/30 Stacks Part 1/Stacks.java
@@ -1,5 +1,16 @@
 import java.util.Stack;
 public class Stacks {
+
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop(); 
+        pushAtBottom(s, data); 
+        s.push(top); 
+    }
+
     public static void main(String args[]) {
         Stack<Integer> s = new Stack<>();
 
@@ -8,9 +19,10 @@ public class Stacks {
         s.push(3);
         s.push(4);
 
+        pushAtBottom(s, 5); 
+
         while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
+            System.out.println(s.pop());
         }
     }
 }
