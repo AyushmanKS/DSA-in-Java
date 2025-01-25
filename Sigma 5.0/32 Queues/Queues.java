50e5ee5b95abbcf562b66b23478df5e93d7b7640
AyushmanKS <ayushmanks1845@gmail.com>
2025-01-25 10:46:08 +0530

    fix: queue using array

--- Sigma 5.0/32 Queues/Queues.java
+++ Sigma 5.0/32 Queues/Queues.java
@@ -1,8 +1,8 @@
 public class Queues {
     static class Queue {
-        static int arr[];
-        static int size;
-        static int rear;
+        int arr[];  
+        int size;   
+        int rear;  
     
         Queue(int n) {
             arr = new int[n];
@@ -10,11 +10,11 @@ public class Queues {
             rear = -1;
         }
 
-        public static boolean isEmpty() {
+        public boolean isEmpty() {
             return rear == -1;
         }
 
-        public static void add(int data) {
+        public void add(int data) {
             if(rear == size - 1) {
                 System.out.println("queue is full");
                 return;
@@ -23,7 +23,7 @@ public class Queues {
             arr[rear] = data;
         }
 
-        public static int remove() {
+        public int remove() {
             if(isEmpty()) {
                 System.out.println("empty queue");
                 return -1;
@@ -36,7 +36,7 @@ public class Queues {
             return front;
         }
 
-        public static int peek() {
+        public int peek() {
             if(isEmpty()) {
                 System.out.println("empty queue");
                 return -1;
@@ -44,17 +44,20 @@ public class Queues {
             return arr[0];
         }
     }   
-    public static void main(String args[]) {
-        //Queue q = new Queue(5);
-        Queue.add(1);
-        Queue.add(2);
-        Queue.add(3);
-        Queue.add(4);
-        Queue.add(5);
 
-        while(!Queue.isEmpty()) {
-            System.out.println(Queue.peek());
-            Queue.remove();
+    public static void main(String args[]) {
+        // Create a Queue instance with size 5
+        Queue q = new Queue(5);
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        while(!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
         }
-    } 
+    }
 }
