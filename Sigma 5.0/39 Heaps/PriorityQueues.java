2790e532b4283956fa71bbe5bb83e7c6dd4c14cc
AyushmanKS <ayushmanks1845@gmail.com>
2025-02-23 20:21:59 +0530

    student class using priority queue

--- Sigma 5.0/39 Heaps/PriorityQueues.java
+++ Sigma 5.0/39 Heaps/PriorityQueues.java
@@ -2,14 +2,28 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class PriorityQueues{
+
+    static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+    }
     public static void main(String args[]) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(1);
-        pq.add(2);
-        pq.add(3);
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student("A", 10));
+        pq.add(new Student("B", 1));
+        pq.add(new Student("C", 9));
 
         while(!pq.isEmpty()) {
-            System.out.print(pq.peek()+" ");
+            System.out.println(pq.peek().name+" "+pq.peek().rank);
             pq.remove();
         }
     }
