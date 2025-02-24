755a44e4f9862407a1a322726dce6a851ff91b14
AyushmanKS <ayushmanks1845@gmail.com>
2025-02-24 14:24:57 +0530

    remove and heapify

--- Sigma 5.0/39 Heaps/Heaps.java
+++ Sigma 5.0/39 Heaps/Heaps.java
@@ -19,7 +19,54 @@ public class Heaps {
                 // Update indices
                 child = parent;
                 parent = (child - 1) / 2;
+
+                child = parent;
+                parent = (child-1) / 2;
             }
+        }
+
+        public int remove() {
+            int data = arr.get(0);
+
+            // swap first and last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size()-1));
+            arr.set(arr.size()-1, temp);
+
+            // delete last
+            arr.remove(arr.size()-1);
+
+            // heapify
+            heapify(0);
+
+            return data;
+        }
+
+        private void heapify(int i) {
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+            int min = i;
+
+            if(left < arr.size() && arr.get(min) > arr.get(left)) {
+                min = left;
+            }
+
+            if(right < arr.size() && arr.get(min) > arr.get(right)) {
+                min = right;
+            }
+
+            if(min != i) {
+                // swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(min));
+                arr.set(min, temp);
+
+                heapify(min);
+            }
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
         }
 
         // Print the heap as a list
