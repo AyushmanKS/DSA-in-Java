1375512664330ba7e938a5e0d4e12440b6f1e97f
AyushmanKS <ayushmanks1845@gmail.com>
2025-03-01 10:38:31 +0530

    hashmap basic operations

---
+++ Sigma 5.0/40 Hashing/HashMaps.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class HashMaps {
+    public static void main(String args[]) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        
+        hm.put("India", 100);
+        hm.put("China", 150);
+        hm.put("US", 50);
+
+        System.out.println(hm);
+
+        System.out.println(hm.get("China"));
+
+        System.out.println(hm.containsKey("India"));
+
+        System.out.println(hm.remove("China"));
+        System.out.println(hm);
+
+        System.out.println(hm.size());
+        
+        hm.clear();
+        System.out.println(hm.isEmpty());
+    }
+}
