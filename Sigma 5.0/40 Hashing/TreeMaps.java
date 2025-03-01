c786fa17183fa17eb7d856c1f12443a479855f9b
AyushmanKS <ayushmanks1845@gmail.com>
2025-03-01 14:47:10 +0530

    tree maps creation

---
+++ Sigma 5.0/40 Hashing/TreeMaps.java
@@ -0,0 +1,14 @@
+import java.util.TreeMap;
+
+public class TreeMaps {
+    public static void main(String args[]) {
+        
+        TreeMap<String, Integer> thm = new TreeMap<>();
+
+        thm.put("India", 100);
+        thm.put("China", 150);
+        thm.put("US", 50);
+
+        System.out.println(thm);
+    }
+}
