8429e3021e3551bcb497cf1522c6cccc962e5928
AyushmanKS <ayushmanks1845@gmail.com>
2025-03-05 23:11:56 +0530

    trie basics

---
+++ Sigma 5.0/41 Trie/Tries.java
@@ -0,0 +1,35 @@
+public class Tries {
+
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false; // end of word
+
+        Node() {
+            for(int i=0; i<26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root;
+
+    public static void insert(String word) {
+        Node curr = root;
+        for(int level = 0; level<word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static void main(String args[]) {
+        String words[] = {"the", "a", "there", "their", "any", "thee"};
+        
+        for(int i=0; i<words.length; i++) {
+            insert(words[i]);
+        }
+    }
+}
