182e4ed7bc2d9c048bdc31cde33fc148cb26028a
AyushmanKS <ayushmanks1845@gmail.com>
2025-03-18 19:57:02 +0530

    cycle detection in graph

---
+++ Sigma 5.0/42 Graphs Part 1/CycleDetection.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+public class CycleDetection {
+    
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static boolean detectCycle(ArrayList<Edge>[] graph) {
+        boolean visited[] = new boolean[graph.length];
+
+        for(int i=0; i<graph.length; i++) {
+            if(!visited[i]) {
+                if(detectCycleUtil(graph, visited, i, -1)) return true; 
+            }
+        }
+        return false;
+    }
+
+    public static boolean detectCycleUtil(ArrayList<Edge>[] graph, boolean[] visited, int curr, int par) {
+        visited[curr] = true;
+
+        for(Edge e : graph[curr]) {
+            if(!visited[e.dest]) {
+                if(detectCycleUtil(graph, visited, e.dest, curr)) {
+                    return true;
+                }
+            } 
+            else if(e.dest != par) { 
+                return true;
+            }
+        }
+        return false;
+    } 
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for(int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        
+        graph[0].add(new Edge(0, 1));
+        graph[1].add(new Edge(1, 0));
+
+        graph[0].add(new Edge(0, 2));
+        graph[2].add(new Edge(2, 0));
+
+        graph[0].add(new Edge(0, 3));
+        graph[3].add(new Edge(3, 0));
+
+        graph[1].add(new Edge(1, 2));
+        graph[2].add(new Edge(2, 1));
+
+        graph[3].add(new Edge(3, 4));
+        graph[4].add(new Edge(4, 3));
+    }
+
+    public static void main(String args[]) {
+        int V = 5;
+        
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge>[] graph = new ArrayList[V];
+
+        createGraph(graph);
+
+        System.out.println("Cycle exists: " + detectCycle(graph));
+    }
+}
