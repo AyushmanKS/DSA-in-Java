073ac93dd767f66d2fbe19b7d2f74671ea9ae642
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-06 12:22:52 +0530

    climbing stairs tabulation

--- Sigma 5.0/48 DP Part 1/ClimbingStairs.java
+++ Sigma 5.0/48 DP Part 1/ClimbingStairs.java
@@ -18,6 +18,21 @@ public class ClimbingStairs {
         ways[n] = memorization(n-1, ways) + memorization(n-2, ways);
         return ways[n];
     }
+
+    public static int tabulation(int n, int ways[]) {
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+
+        for(int i=1; i<=n; i++) {
+            if(i==1) {
+                dp[i] = dp[i-1];
+            }
+            else {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
+    }
     public static void main(String args[]) {
         int n = 5;
         System.out.println(climbStairs(n));
@@ -25,5 +40,7 @@ public class ClimbingStairs {
         int ways[] = new int[n+1];
         Arrays.fill(ways, -1);
         System.out.println(memorization(n, ways));
+
+        System.out.println(tabulation(n, ways));
     }
 }
