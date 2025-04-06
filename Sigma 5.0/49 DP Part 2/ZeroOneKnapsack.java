6e6817a03ba1e529b106ac54b5924330f58117da
AyushmanKS <ayushmanks1845@gmail.com>
2025-04-06 15:58:54 +0530

    zero one knapsack memoization

--- Sigma 5.0/49 DP Part 2/ZeroOneKnapsack.java
+++ Sigma 5.0/49 DP Part 2/ZeroOneKnapsack.java
@@ -1,11 +1,15 @@
+import java.util.Arrays;
+
 public class ZeroOneKnapsack {
 
     public static int knapsack(int val[], int wt[], int w, int n) {
         if(w == 0 || n == 0) return 0;
 
         if(wt[n-1] <= w) {
+            // include
             int ans1 = val[n-1] + knapsack(val, wt, w-wt[n-1], n-1);
 
+            // exclude
             int ans2 = knapsack(val, wt, w, n-1);
 
             return Math.max(ans1, ans2);
@@ -15,11 +19,41 @@ public class ZeroOneKnapsack {
         }
     }
 
+    public static int memoization(int val[],int wt[], int w, int n, int dp[][]) {
+        if(w==0 || n==0) return 0;
+
+        if(dp[n][w] != -1) {
+            return dp[n][w];
+        }
+
+        if(wt[n-1] <= w) {
+            // include
+            int ans1 = val[n-1] + memoization(val, wt, w-wt[n-1], n-1, dp);
+
+            //exclude
+            int ans2 = memoization(val, wt, w, n-1, dp);
+
+            dp[n][w] =  Math.max(ans1, ans2);
+            return dp[n][w];
+        }
+        else {
+            dp[n][w] = memoization(val, wt, w, n-1, dp);
+            return dp[n][w];
+        }
+    }
+
     public static void main(String args[]) {
         int val[] = {15,14,10,45,30};
         int wt[] = {2,5,1,3,4};
         int w = 7;
 
+        int dp[][] = new int[val.length+1][w+1];
+        for (int i = 0; i <= val.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
         System.out.println(knapsack(val, wt, w, val.length));
+
+        System.out.println(memoization(val, wt, w, val.length, dp));
     }
 }
